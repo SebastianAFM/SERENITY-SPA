e944f05cd61fff8803a83205b3b139961c6e0bc0
SebastianAFM <safernandm@eafit.edu.co>
2026-05-21 16:52:40 -0500

    calajo

--- scripts/database.js
+++ scripts/database.js
@@ -1,8 +1,7 @@
 // Configuración de Supabase
 // REEMPLAZA ESTOS VALORES CON LOS DE TU PROYECTO EN SUPABASE.COM
 const SUPABASE_URL = "https://aonkerizxolmeizhizwp.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFvbmtlcml6eG9sbWVpemhpendwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg4NzU4MDIsImV4cCI6MjA5NDQ1MTgwMn0.9awKi3grvNO7VP70gaCMQN_76SLTzCLLmHjdI4jNnlc";
-
+const SUPABASE_ANON_KEY = "sb_publishable_rlrPOkZ3hms2iGJMEbqX6w_EQZi3iG5";
 // Usamos window.supabase para evitar la referencia circular con la constante
 const supabase = window.supabase ? window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY) : null;
 
